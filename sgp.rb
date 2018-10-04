bda0d5d4b459f82952213e4864d385e4a5daa869
Micke <micke@nimell.se>
2018-10-04 21:43:04 +0200

    better handling of ARGV

--- sgp.rb
+++ sgp.rb
@@ -5,26 +5,19 @@ require 'io/console'
 	 hash password generation. 2018 by mnl@vandal.nu
 	 Written as an excercise. Outputs to stdout
 =end
-unless ARGV[0] 
+domain, len, $dig = ARGV
+
+unless domain
 	puts "Usage #$0 [domainname] [length (optional)]\n"; exit 1;
 end
-domain = ARGV[0]
-case ARGV[1].to_i # Password length
-when 1..50
-	len = ARGV[1].to_i
-else
-	len = 15 # Default pass length
-end
+#Set password length to 15 is not user defined
+len = (len.to_i.equal? 0) ? 15 : len.to_i
 
-def dig(h)
-case ARGV[2]
-when "sha512"
-	return Digest::SHA512.base64digest h
-when "md5",nil #md5 is default
-	return Digest::MD5.base64digest h
-else
-	puts "Unknown digest: #{ARGV[2]}"; exit
-end
+case $dig
+  when "md5",nil then $dig = Digest::MD5 #md5 is default
+  when "sha512" then $dig = Digest::SHA512
+  else
+	  puts "Unknown digest: #{$dig}"; exit 1
 end
 
 print "Password: "
@@ -41,8 +34,8 @@ end
 
 $i = 0
 until valid(hash) do
-	hash = dig(hash)
-	hash = hash.tr('+/=','98A')
+	hash = $dig.base64digest(hash)
+	hash.tr!('+/=','98A')
 	$i += 1
 end
 
