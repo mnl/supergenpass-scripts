e6ddda5549d6f593919ee32d38aa9c11c553ae38
Micke <micke@nimell.se>
2019-02-12 23:47:37 +0100

    Implemented hashing and validation. Only MD5 so far

--- sgp.tcl
+++ sgp.tcl
@@ -6,35 +6,36 @@
 package require md5
 
 if {$argc < 1} {
-	puts stderr "Usage $argv0 domainname \[length] \[digest]"
+	puts stderr "Usage $argv0 domainname \[length]"
 	exit 1 ;# Quit
 }
 set domain [lindex $argv 0]
 set len [expr {$argc > 1 ? [lindex $argv 1] : 15}] ;# Default length
-set dig [expr {$argc > 2 ? [lindex $argv 2] : "md5"}] ;# Digest 
-switch $dig {
-	"sha512" {
-		set dig "sha"
-	}
-	"md5" {
-		set dig "md5"	
-	}
-	default {
-		puts stderr "Unsupported digest: $dig"
-		exit 1
-	}
-}
-#set pass "hejsan"
+proc dig {arg} {md5::md5 $arg} ;# Only MD5 support right now :(
+
 exec stty -echo ;# Hide input (unix only)
 puts -nonewline "Password: "
 flush stdout
 gets stdin pass
 exec stty echo ;# turn on echo again
 exec tput cr >@stdout <@stdin ;# move cursor to the left
-set hash "$pass:$domain"
-proc makeHash {hash} {
-	set hash [binary encode base64 [md5::md5 $hash]]
-	return [string map { + 9 / 8 = A } $hash]
+
+proc valid {hash} {
+# Hash should begin with lowercase and contain numbers and uppercase
+	if {
+		[string match {[A-Z0-9]*} $hash] ||
+		[string is alpha $hash] || 
+		! [string match {*[A-Z]*} $hash]
+	} {return 0}
+	return 1 ;# String is valid
 }
-set hash [makeHash $hash]
-puts [string range $hash 0 $len] ;# Cut to length and print
+
+proc rehash {hash times} {
+	if { $times < 0 && [valid $hash] } { return $hash }
+	set hash [string map { + 9 / 8 = A } [binary encode base64 [dig $hash]]]
+	return [rehash $hash [expr $times - 1]]
+}
+set hash [rehash "$pass:$domain" 10] ;# hash ten times
+set max [expr {$len > [string length $hash] ? [string length $hash] : $len}]
+puts [string range $hash 0 $max] ;# Cut to length and print
+
